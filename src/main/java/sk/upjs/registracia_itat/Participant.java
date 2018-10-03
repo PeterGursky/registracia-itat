dd86960c37593875b03a114103181c6a884c0783
student <student@158.197.35.78>
2018-10-03 08:17:05 +0200

    cviko 3 - prestavka

--- src/main/java/sk/upjs/registracia_itat/Participant.java
+++ src/main/java/sk/upjs/registracia_itat/Participant.java
@@ -4,6 +4,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Participant {
+	
+	public static final double SINGLE_ROOM_FEE = 75.0;
+	public static final double[] COMPANION_FEES = new double[]{60.0, 110.0, 165.0, 210.0};
+	public static final double BANKET_FEE = 20.0;
+	public static final LocalDateTime BANKET_DATETIME 
+	 = LocalDateTime.of(2019, 9, 23, 19, 0);
+	
 	private String name;
 	private String surname;
 	private String email;
@@ -20,4 +27,151 @@ public class Participant {
 	private List<Companion> companions;
 	private WorkShop workshop;
 	private boolean cash;
+	
+	public double finalPrice() {
+		double price = 0.0;
+		if (this.early) {
+			if (this.student) {
+				price = workshop.getPriceStudent();
+			} else {
+				price = workshop.getPriceFull();
+			}
+		} else {
+			if (this.student) {
+				price = workshop.getPriceStudentLate();
+			} else {
+				price = workshop.getPriceFullLate();
+			}
+		}
+		
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getIco() {
+		return ico;
+	}
+
+	public void setIco(Long ico) {
+		this.ico = ico;
+	}
+
+	public String getDic() {
+		return dic;
+	}
+
+	public void setDic(String dic) {
+		this.dic = dic;
+	}
+
+	public boolean isEarly() {
+		return early;
+	}
+
+	public void setEarly(boolean early) {
+		this.early = early;
+	}
+
+	public Tshirt getTshirt() {
+		return tshirt;
+	}
+
+	public void setTshirt(Tshirt tshirt) {
+		this.tshirt = tshirt;
+	}
+
+	public boolean isStudent() {
+		return student;
+	}
+
+	public void setStudent(boolean student) {
+		this.student = student;
+	}
+
+	public boolean isSingleRoom() {
+		return singleRoom;
+	}
+
+	public void setSingleRoom(boolean singleRoom) {
+		this.singleRoom = singleRoom;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public List<Companion> getCompanions() {
+		return companions;
+	}
+
+	public void setCompanions(List<Companion> companions) {
+		this.companions = companions;
+	}
+
+	public WorkShop getWorkshop() {
+		return workshop;
+	}
+
+	public void setWorkshop(WorkShop workshop) {
+		this.workshop = workshop;
+	}
+
+	public boolean isCash() {
+		return cash;
+	}
+
+	public void setCash(boolean cash) {
+		this.cash = cash;
+	}
 }
---
+++ src/test/java/sk/upjs/registracia_itat/ParticipantTest.java
@@ -0,0 +1,76 @@
+package sk.upjs.registracia_itat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParticipantTest {
+
+	WorkShop cidmWorkshop;
+	WorkShop slonlpWorkshop;
+	
+	public ParticipantTest() {
+		cidmWorkshop = new WorkShop();
+		cidmWorkshop.setPriceFull(355);
+		cidmWorkshop.setPriceStudent(295);
+		cidmWorkshop.setPriceFullLate(380);
+		cidmWorkshop.setPriceStudentLate(320);
+		slonlpWorkshop = new WorkShop();
+		slonlpWorkshop.setPriceFull(245);
+		slonlpWorkshop.setPriceStudent(195);
+		slonlpWorkshop.setPriceFullLate(265);
+		slonlpWorkshop.setPriceStudentLate(210);
+	}
+	
+	@Test
+	void testFinalPrice1() {
+		Participant participant = new Participant();
+		participant.setEarly(true);
+		participant.setWorkshop(cidmWorkshop);
+		participant.setStudent(false);
+		participant.setSingleRoom(false);
+		participant.setCash(false);
+		System.out.println(participant.finalPrice());
+		
+		assertEquals(355.0, participant.finalPrice());
+	}
+
+	@Test
+	void testFinalPrice2() {
+		Participant participant = new Participant();
+		participant.setEarly(false);
+		participant.setWorkshop(cidmWorkshop);
+		participant.setStudent(false);
+		participant.setSingleRoom(false);
+		participant.setCash(false);
+		assertEquals(380.0, participant.finalPrice());
+	}
+
+	@Test
+	void testFinalPrice3() {
+		Participant participant = new Participant();
+		participant.setEarly(true);
+		participant.setWorkshop(slonlpWorkshop);
+		participant.setStudent(false);
+		participant.setSingleRoom(false);
+		participant.setCash(false);
+		List<Companion> companions = new ArrayList<>();
+		Companion c1 = new Companion();
+		c1.setCategory(CompanionCategory.ADULT);
+		c1.setStart(LocalDateTime.of(2019, 9, 21, 17, 0));
+		c1.setEnd(LocalDateTime.of(2019, 9, 23, 10, 0));
+		c1.setTshirt(null);
+		companions.add(c1);
+		participant.setCompanions(companions);
+		assertEquals(245 + 110, participant.finalPrice());
+	}
+
+}
